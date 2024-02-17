b3792cf07d6d510e41f6d4fbd730e790604cf074
Shreyas Chate <chateshreyas231@gmail.com>
2024-02-17 03:15:13 -0600

    confirm

--- zzz/z/app/(tabs)/NewPostScreen.tsx
+++ zzz/z/app/(tabs)/NewPostScreen.tsx
@@ -3,11 +3,12 @@ import { StyleSheet } from 'react-native';
 import EditScreenInfo from '../../components/EditScreenInfo';
 import { Text, View } from '../../components/Themed';
 
-export default function TabOneScreen() {
+export default function() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      </View>
+      <Text style={styles.title}>New Post</Text>
+      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    </View>
   );
 }
 
